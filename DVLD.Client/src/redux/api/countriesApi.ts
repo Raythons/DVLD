e0722f546abaaa9fe834bc3ae8e30e7c5f75b899
Raython <rasheed998855@gmail.com>
2024-06-30 05:27:34 +0300

    Added CountriesApi

---
+++ DVLD.Client/src/redux/api/countriesApi.ts
@@ -0,0 +1,46 @@
+import { apiSlice } from "./apiSlice";
+import { CreatePersonFormFields } from "../../types/AddPersonType";
+// import { handleRtkQueryErrors } from "../helpers";
+export const PeopleEndPoint = "Person"
+
+
+
+
+export type ApiError = {
+    Code?: string,
+    Message: string,
+    Reasons: string[],
+    status?: number;
+}
+
+export type MainApiResponse  =  {
+    Errors: ApiError[]
+    IsSuccess: boolean,
+    Response: unknown,
+}
+
+type CreatePersonBody = CreatePersonFormFields;
+
+// type EditPersonBody = CreatePersonBody;
+export type Country = {
+    Id: number,
+    Country: string,
+} 
+export const CountryEndPoint = "country"
+
+
+export type getPersonEditDetailsResponse = CreatePersonBody
+
+export const CountriesApi =  apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllCountries: builder.query<Country[], undefined>({
+                query:  () => ({
+                    url: `${CountryEndPoint}`,
+                })
+            
+        })
+        // editPerson: builder.mutation<boolean, EditPersonBody>( )
+    })
+})
+
+export const {useGetAllCountriesQuery} = CountriesApi;
